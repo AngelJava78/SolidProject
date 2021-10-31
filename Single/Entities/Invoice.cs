490768764c0be02a2fef1851d0bec963475bb0d9
Angel Javier <angel_java@hotmail.com>
2021-10-31 12:48:03 -0600

    Before to apply the Single Responsibility Principle

---
+++ Single/Entities/Invoice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single.Entities
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public List<Item> Items { get; set; }
+        public decimal GetTotal()
+        {
+            var total = 0.0m;
+            foreach(var item in Items)
+            {
+                total += item.Quantity * item.Product.Price;
+            }
+            return total;
+        }
+    }
+}
